b0f9692465a865bf58df64b0c5f9c75c4a81896b
Marcel <marceltubben@gmail.com>
2023-06-13 11:13:20 +0200

    added seed

---
+++ src/main/java/com/project1CATNIP/CATNIP/controller/SeedController.java
@@ -0,0 +1,40 @@
+package com.project1CATNIP.CATNIP.controller;
+
+/*
+ *@Author: Marcel Tubben <mhg.tubben@st.hanze.nl>
+ *
+ *The Purpose
+ */
+
+import com.project1CATNIP.CATNIP.model.Teacher;
+import com.project1CATNIP.CATNIP.repository.TeacherRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class SeedController {
+
+    private final TeacherRepository teacherRepository;
+
+    @GetMapping("/seed")
+    private String seedDatabase() {
+
+        Teacher wally = new Teacher();
+        wally.setFirstName("Walther");
+        wally.setInfixName("De");
+        wally.setLastName("Wit");
+        wally.setEmailAddress("w.d.wit@cat-nip.nl");
+        teacherRepository.save(wally);
+
+        Teacher piet = new Teacher();
+        wally.setFirstName("Pieter");
+        wally.setInfixName("Van de");
+        wally.setLastName("Breedband");
+        wally.setEmailAddress("p.vd.breedband@cat-nip.nl");
+        teacherRepository.save(piet);
+
+        return "redirect:/teacher";
+    }
+}
