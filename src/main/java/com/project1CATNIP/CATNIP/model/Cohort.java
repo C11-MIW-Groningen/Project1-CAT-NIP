ac7f947bc5bc13ac7f053685c6e3e8eba78169e0
Saskia Tadema <saskiatadema1@hotmail.com>
2023-06-16 11:19:40 +0200

    added multiple column unique constraint to cohort

--- src/main/java/com/project1CATNIP/CATNIP/model/Cohort.java
+++ src/main/java/com/project1CATNIP/CATNIP/model/Cohort.java
@@ -15,6 +15,9 @@ import java.util.List;
 
 @Entity
 @Getter @Setter
+@Table(uniqueConstraints={
+        @UniqueConstraint(columnNames = {"number", "program_program_id"})
+})
 public class Cohort {
 
     @Id @GeneratedValue
